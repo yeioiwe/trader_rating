7873b2d91ccff9bf9c37b2036df1595b74f6e3cd
yeioiwe <yolog998@protonmail.com>
2025-02-26 21:45:14 +0100

    update mui theme breakpoints

--- web/shared/config/theme/theme.ts
+++ web/shared/config/theme/theme.ts
@@ -11,7 +11,15 @@ const theme = createTheme({
             default: '#FFFFF',
         },
     },
-
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 900,
+            lg: 1280,
+            xl: 1536,
+        },
+    },
     typography: {
         allVariants: font.style,
     },
