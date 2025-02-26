bd39edcc6b2b2fba8ee9f55b071acc25ac72f179
yeioiwe <yolog998@protonmail.com>
2025-02-26 17:09:28 +0100

    svgr nextjs config

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,6 +3,15 @@ const { i18n } = require('./next-i18next.config');
 
 const nextConfig: NextConfig = {
     i18n,
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
 };
 
 export default nextConfig;
