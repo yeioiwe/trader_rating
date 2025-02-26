8581eba08704c4bd026e65013d6b8760df61f202
yeioiwe <yolog998@protonmail.com>
2025-02-26 21:44:58 +0100

    update layout: add header / sidebar

--- web/app/app.layout.tsx
+++ web/app/app.layout.tsx
@@ -1,13 +1,24 @@
 'use client';
 import theme from '@/shared/config/theme/theme';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Col, Row } from '@/shared/ui/boxes';
+import { HeaderMain } from '@/view/layout/header/header.main';
+import { SideBarMain } from '@/view/layout/sidebar/sidebar.main';
+import { Container, CssBaseline, ThemeProvider } from '@mui/material';
 import React from 'react';
 
 export const AppLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            {children}
+            <Col>
+                <HeaderMain />
+                <Container maxWidth="lg">
+                    <Row justifyContent={'space-between'} alignItems={'flex-start'}>
+                        {children}
+                        <SideBarMain />
+                    </Row>
+                </Container>
+            </Col>
         </ThemeProvider>
     );
 };
