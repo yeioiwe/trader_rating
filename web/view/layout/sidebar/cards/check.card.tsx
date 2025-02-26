3ffa6e3108a4310808534f7abc444ad038a9d569
yeioiwe <yolog998@protonmail.com>
2025-02-27 00:17:23 +0100

    sidebar check card

---
+++ web/view/layout/sidebar/cards/check.card.tsx
@@ -0,0 +1,31 @@
+import CheckIcon from '@/public/icons/layout_check.svg';
+import { Col } from '@/shared/ui/boxes';
+import { Button, Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { SidebarCard } from './card';
+
+export const CheckCard = () => {
+    const { t } = useTranslation();
+
+    return (
+        <SidebarCard bgcolor={'#ECF2FF'} icon={<CheckIcon />}>
+            <Col gap={2}>
+                <Col gap={0.5}>
+                    <Typography fontSize={20} fontWeight={700}>
+                        {t('sidebar.check.title')}
+                    </Typography>
+
+                    <Typography color="#B8BDBA" fontSize={16}>
+                        {t('sidebar.check.description')}
+                    </Typography>
+                </Col>
+
+                <Button sx={{ bgcolor: '#37E585', color: '#FFFFFF', minHeight: '50px', borderRadius: '9px' }}>
+                    <Typography fontSize={20} fontWeight={700}>
+                        {t('sidebar.check.button')}
+                    </Typography>
+                </Button>
+            </Col>
+        </SidebarCard>
+    );
+};
