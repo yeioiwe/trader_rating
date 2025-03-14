7ef3d547972d96cccd664e3370e3dd8b99ba386c
yeioiwe <yolog998@protonmail.com>
2025-03-14 17:33:27 +0100

    scammers_list: main page / title

---
+++ web/view/scammers_list/scammers_list.main.tsx
@@ -0,0 +1,18 @@
+import { Col } from '@/shared/ui/boxes';
+import { Banner } from '../main/banner/banner';
+import { ScammerProjectsList } from './projects/scammers.projects.list';
+import { ScammersListAbout } from './scammers_list.about';
+import { ScammersListPosts } from './scammers_list.posts';
+import { ScammersListTitle } from './scammers_list.title';
+
+export const ScammersListMain = () => {
+    return (
+        <Col gap={4} width={'100%'} flexGrow={1}>
+            <Banner />
+            <ScammersListTitle />
+            <ScammerProjectsList />
+            <ScammersListPosts />
+            <ScammersListAbout />
+        </Col>
+    );
+};
---
+++ web/view/scammers_list/scammers_list.title.tsx
@@ -0,0 +1,23 @@
+import { Col } from '@/shared/ui/boxes';
+import { Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+
+export const ScammersListTitle = () => {
+    const { t } = useTranslation();
+
+    return (
+        <Col gap={1}>
+            <Typography fontSize={30} fontWeight={700}>
+                Список мошенников
+            </Typography>
+            <Typography fontSize={16} color="#918C8C">
+                Обновлено: 17 Янв, 08:42
+            </Typography>
+            <Typography fontSize={16} color="#3B3B3B">
+                В мире трейдинга, особенно в сфере криптовалют, существует немало мошенников, которые обещают быструю
+                прибыль, но в итоге обманывают инвесторов. Чтобы уберечь вас от финансовых потерь, мы собрали список
+                недобросовестных трейдеров, подозрительных платформ и мошеннических схем.
+            </Typography>
+        </Col>
+    );
+};
