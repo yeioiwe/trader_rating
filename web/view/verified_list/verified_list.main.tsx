22d64df8f1f44c3831c266664d4b299ad9e39e65
yeioiwe <yolog998@protonmail.com>
2025-03-14 16:36:33 +0100

    verified page main and title

---
+++ web/view/verified_list/verified_list.main.tsx
@@ -0,0 +1,18 @@
+import { Col } from '@/shared/ui/boxes';
+import { Banner } from '../main/banner/banner';
+import { VerfiedProjectsList } from './projects/verified.projects.list';
+import { VerifiedListBenefits } from './verified_list.benefits';
+import { VerifiedListPosts } from './verified_list.posts';
+import { VerifiedListTitle } from './verified_list.title';
+
+export const VerifiedListMain = () => {
+    return (
+        <Col gap={4} width={'100%'} flexGrow={1}>
+            <Banner />
+            <VerifiedListTitle />
+            <VerfiedProjectsList />
+            <VerifiedListPosts />
+            <VerifiedListBenefits />
+        </Col>
+    );
+};
---
+++ web/view/verified_list/verified_list.title.tsx
@@ -0,0 +1,24 @@
+import { Col } from '@/shared/ui/boxes';
+import { Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+
+export const VerifiedListTitle = () => {
+    const { t } = useTranslation();
+
+    return (
+        <Col gap={1}>
+            <Typography fontSize={30} fontWeight={700}>
+                Рейтинг провервенных трейдеров
+            </Typography>
+            <Typography fontSize={16} color="#918C8C">
+                Обновлено: 17 Янв, 08:42
+            </Typography>
+            <Typography fontSize={16} color="#3B3B3B">
+                Хотите уверенно инвестировать в криптовалюту и следить за лучшими трейдерами? Мы собрали топ самых
+                опытных и надежных криптовалютных трейдеров. Лучшие трейдеры – с высокими результатами, проверенными
+                стратегиями и положительными отзывами. Оценки экспертов и реальных пользователей – объективная
+                информация без рекламы.
+            </Typography>
+        </Col>
+    );
+};
