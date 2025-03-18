b7e682dab42a8f4ba799fba7f92919c52c64b955
yeioiwe <yolog998@protonmail.com>
2025-03-18 14:37:30 +0100

    dashboard page init

---
+++ admin/app/(auth)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+import { DashboardMain } from '@/view/dashboard/dashboard.main';
+
+export default function DashboardPage() {
+    return <DashboardMain />;
+}
---
+++ admin/view/dashboard/dashboard.main.tsx
@@ -0,0 +1,5 @@
+import { Typography } from '@mui/material';
+
+export const DashboardMain = () => {
+    return <Typography>Dashboard page</Typography>;
+};
