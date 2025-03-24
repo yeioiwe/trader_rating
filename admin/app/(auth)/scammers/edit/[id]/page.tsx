b20b61762cfe193ff27d52df5e00f124c02d0227
yeioiwe <yolog998@protonmail.com>
2025-03-24 18:48:47 +0100

    init edit page

--- admin/app/(auth)/scammers/edit/[id]/page.tsx
+++ admin/app/(auth)/scammers/edit/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { ScammersEdit } from '@/view/scammers/edit/scammers.edit';
 
-export default function ScammersEditPage() {
-    return <ScammersEdit />;
+export default async function ScammersEditPage({ params }: { params: Promise<{ id: number }> }) {
+    const id = (await params).id;
+    return <ScammersEdit id={id} />;
 }
