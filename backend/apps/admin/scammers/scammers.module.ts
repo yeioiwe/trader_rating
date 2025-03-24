a1da54734bb623e08ec6b04188571f06e8ac065b
yeioiwe <yolog998@protonmail.com>
2025-03-24 16:38:05 +0100

    add metadata entity scammers module

--- backend/apps/admin/scammers/scammers.module.ts
+++ backend/apps/admin/scammers/scammers.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScammerEntity } from 'apps/libs/db/entity/scammer.entity';
 import { ScammersController } from './scammers.controller';
 import { ScammersService } from './scammers.service';
 
 @Module({
-    imports: [JwtModule, TypeOrmModule.forFeature([])],
+    imports: [JwtModule, TypeOrmModule.forFeature([ScammerEntity])],
     providers: [ScammersService],
     controllers: [ScammersController],
 })
