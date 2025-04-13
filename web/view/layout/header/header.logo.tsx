86e4d56003abb4c04e82e515c53d7148bf65fd9c
yeioiwe <yolog998@protonmail.com>
2025-04-13 15:18:12 +0200

    add onClick logo main page route

--- web/view/layout/header/header.logo.tsx
+++ web/view/layout/header/header.logo.tsx
@@ -1,10 +1,14 @@
+'use client';
 import Logo from '@/public/logo.svg';
 import { Row } from '@/shared/ui/boxes';
+import { useRouter } from 'next/navigation';
 import { HederYoutubeBanner } from './header.banners';
 
 export const HeaderLogo = () => {
+    const router = useRouter();
+
     return (
-        <Row justifyContent={'space-between'}>
+        <Row justifyContent={'space-between'} onClick={() => router.push('/')} sx={{ cursor: 'pointer' }}>
             <Logo />
             <HederYoutubeBanner />
         </Row>
