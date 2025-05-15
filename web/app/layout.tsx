dd65c68c92554e6885b07d7409057e8a0e319d21
yeioiwe <yolog998@protonmail.com>
2025-05-15 11:01:52 +0200

    add gtag

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from 'next';
 import { AppLayout } from './app.layout';
 import ChatLayout from './chat';
+import Script from 'next/script';
+
+const GA_MEASUREMENT_ID = 'G-8MNRF9T3XL';
 
 export const metadata: Metadata = {
     title: 'ANTI SCAMER',
-    description: 'Description init page',
+    description: 'Информационная платформа, ориентированная на изучение и анализ онлайн-ресурсов, связанных с финансовой активностью в интернете. Предоставляем пользователям достоверные сведения для понимания сути и структуры цифровых проектов, сайтов и площадок.',
 };
 
 export default function RootLayout({
@@ -16,6 +19,21 @@ export default function RootLayout({
         <html lang="ru">
             <head>
                 <ChatLayout />
+
+                <Script
+                    src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+                    strategy="afterInteractive"
+                    />
+                    <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${GA_MEASUREMENT_ID}', {
+                        page_path: window.location.pathname,
+                        });
+                    `}
+            </Script>
             </head>
             <body>
                 <AppLayout>{children}</AppLayout>
