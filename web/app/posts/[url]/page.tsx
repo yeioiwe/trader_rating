7b3fd611112d8d82c4ec571101036d59479f7515
yeioiwe <yolog998@protonmail.com>
2025-06-14 13:34:54 +0200

    add ssr post page

--- web/app/posts/[url]/page.tsx
+++ web/app/posts/[url]/page.tsx
@@ -3,5 +3,11 @@ import { PostOnePage } from '@/view/post/page/post.page';
 export default async function PostPage({ params }: { params: Promise<{ url: string }> }) {
     const url = (await params).url;
 
-    return <PostOnePage url={url} />;
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/post/one/${url}`);
+    if (!res.ok) {
+        return { notFound: true };
+    }
+
+    const data = await res.json();
+    return <PostOnePage url={url} initialData={data} />;
 }
--- web/view/post/page/post.page.tsx
+++ web/view/post/page/post.page.tsx
@@ -10,9 +10,9 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { PostCommentsList } from './comments/post.comments.main';
 
-export const PostOnePage = ({ url }: { url: string }) => {
+export const PostOnePage = ({ url, initialData }: { url: string; initialData: any }) => {
     const router = useRouter();
-    const { data: post } = usePostGetOne(url);
+    const { data: post } = usePostGetOne(url, initialData);
 
     if (post === undefined) return null;
     return (
